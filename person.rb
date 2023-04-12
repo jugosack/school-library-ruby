6160fcdbab80a3a69b46131950d7b461702a089c
jugosack <jugoslav_ackoski@yahoo.com>
2023-04-12 10:57:54 +0200

    Fixing rubocop erros

--- person.rb
+++ person.rb
@@ -1,11 +1,12 @@
 require './nameable'
 
 class Person < Nameable
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   attr_reader :id
@@ -16,7 +17,7 @@ class Person < Nameable
   end
 
   def correct_name
-@name
+    @name
   end
 
   private
