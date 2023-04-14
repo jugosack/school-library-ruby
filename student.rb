521a27d3175f8af04ca15546865134dbf7957749
jugosack <jugoslav_ackoski@yahoo.com>
2023-04-14 18:58:50 +0200

    Add change in app.rb and main.rb

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(age, classroom, name, parent_permission)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
@@ -17,3 +17,24 @@ class Student < Person
     '¯(ツ)/¯'
   end
 end
+
+# require_relative 'person'
+
+# class Student < Person
+#   attr_reader :classroom
+
+#   def initialize(classroom, age, name, parent_permission)
+#     super(age, name, parent_permission)
+#     @classroom = classroom
+#   end
+
+#   def classroom=(classroom)
+#     @classroom = classroom
+#     classroom.students.push(self) unless classroom.students.include?(self)
+#   end
+
+#   def play_hooky
+#     '¯(ツ)/¯'
+#   end
+# end
+
