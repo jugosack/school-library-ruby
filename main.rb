2054992e64885e2f3d7eaa8b4a98dd51d582f4e9
jugosack <jugoslav_ackoski@yahoo.com>
2023-04-17 15:56:49 +0200

    Add changes for reafctoring main.rb

--- main.rb
+++ main.rb
@@ -1,50 +1,84 @@
 require './app'
 
-def main
-  app = App.new
+class Library
+  def initialize
+    @app = App.new
+  end
 
-  puts '___________________________________________________________'
-  puts 'WELCOME TO THE SCHOOL LIBRARY APP!'
-  puts '___________________________________________________________'
+  def display
+    welcom_mesg
+    loop do
+      list_options
+      option = gets.chomp.to_i
+      process_option(option)
+      puts '___________________________________________________________'
+    end
+  end
 
-  loop do
-    puts list_options
-    option = gets.chomp.to_i
-    option(option, app)
+  private
+
+  def welcom_mesg
+    puts '___________________________________________________________'
+    puts 'WELCOME TO THE SCHOOL LIBRARY APP!'
     puts '___________________________________________________________'
   end
-end
 
-def list_options
-  'Please choose an option by entering a number:
-    1 - List of all books
-    2 - List of all people
-    3 - Add a person
-    4 - Create a book
-    5 - Add a rental
-    6 - List of all rental for given id
-    7 - Exit'
-end
+  def list_options
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List of all books'
+    puts '2 - List of all people'
+    puts '3 - Add a person'
+    puts '4 - Create a book'
+    puts '5 - Add a rental'
+    puts '6 - List of all rental for given id'
+    puts '7 - Exit'
+  end
 
-def option(option, app) # rubocop:disable Metrics/CyclomaticComplexity
-  case option
-  when 1
-    app.list_books
-  when 2
-    app.list_people
-  when 3
-    app.create_person
-  when 4
-    app.create_book
-  when 5
-    app.create_rental
-  when 6
-    app.list_rentals
-  when 7
-    exit
-  else
-    puts 'Invalid option, please type correct number!'
+  def process_option(option) # rubocop:disable Metrics/CyclomaticComplexity
+    if option == 1
+      list_books
+   elsif option == 2
+      list_people
+    elsif option == 3
+      create_person
+    elsif option == 4
+      create_book
+    elsif option == 5
+      create_rental
+    elsif option == 6
+      list_rentals
+    elsif option == 7
+      puts 'Goodbye!'
+      break
+    else
+      puts 'Invalid option, please type correct number!'
+    end
+  end
+
+  def list_books
+    @app.list_books
+  end
+
+  def list_people
+    @app.list_people
+  end
+
+  def create_person
+    @app.create_person
+  end
+
+  def create_book
+    @app.create_book
+  end
+
+  def create_rental
+    @app.create_rental
+  end
+
+  def list_rentals
+    @app.list_rentals
   end
 end
 
-main
+lib = Library.new
+lib.display
