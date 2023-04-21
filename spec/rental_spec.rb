09d96a2a5c0d0efab57b8e0e943abecb2a00639b
jugosack <jugoslav_ackoski@yahoo.com>
2023-04-21 11:15:57 +0300

    Add person_spec.rb rental_spec.rb student_spec.rb teacher_spec.rb

---
+++ spec/rental_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require_relative '../rental'
+require_relative '../person'
+require_relative '../book'
+
+describe Rental do
+  before do
+    @person = Person.new(27, "John Doe", 1)
+    @book = Book.new("The Great Gatsby", "F. Scott Fitzgerald")
+    @rental = Rental.new("2023-04-21", @person, @book)
+  end
+
+  it "has a date" do
+    expect(@rental.date).to eq("2023-04-21")
+  end
+
+  it "belongs to a person" do
+    expect(@rental.person).to eq(@person)
+  end
+
+  it "belongs to a book" do
+    expect(@rental.book).to eq(@book)
+  end
+
+  it "adds itself to the person's rentals" do
+    expect(@person.rentals).to include(@rental)
+  end
+
+  it "adds itself to the book's rentals" do
+    expect(@book.rentals).to include(@rental)
+  end
+end
